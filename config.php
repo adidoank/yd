96b0d32e21e49dbedb0094562dfc494a4cb61881
kingcicak <37271283+kingcicak@users.noreply.github.com>
2019-04-13 21:04:10 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
-"imei" => "xxxxxxxxxxxxx",
-"sign" => "xxxxxxxxxxxxx",
-"token" => "xxxxxxxxxxxxx",
-"uuid" => "xxxxxxxxxxxxx"
+"imei" => "866056045074296",
+"sign" => "1b0e2a6831062c81c6f27ec8b4df44887e9b4c1a",
+"token" => "667a675aff0034183907bd53f7d1523f",
+"uuid" => "365f5bbb79974c6a"
 ];
